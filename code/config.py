62761753770fb205670974c0ec09c9f878b3962d
benwoo1110 <benedict_woo_jun_kai@s2016.sst.edu.sg>
2020-07-28 00:38:26 +0800

    Completed basics UI framework

---
+++ code/config.py
@@ -0,0 +1,82 @@
+######################################
+# Import and initialize the librarys #
+######################################
+import logging
+import yaml
+import os
+
+
+# Default setting for file
+default_config_contents = '''\
+#################################
+# A List of Sorts v2 config.yml #
+#################################
+
+# NOTE: Change only if you know what you are doing!
+
+# Window caption
+title: 'A List of Sorts v2'
+
+# Application icon file location
+icon_file: './icon.png'
+
+# Size of pygame window in pixel
+scale: 1
+
+# Level of output shown
+# CRITICAL -> 50
+# ERROR -> 40
+# WARNING -> 30
+# INFO -> 20
+# DEBUG -> 10
+logging:
+  # For console output
+  console_level: 'INFO'
+  # App activities logged in './A-List-of-Sorts-v2/logs'
+  file_level: 'DEBUG'
+  # Number of logs to keep in logs folder
+  keep_logs: 5
+
+# Changes the refresh rate of pygame
+framerate: 60
+
+# Change speed of scrolling
+scroll_speed: 35
+'''
+
+
+##########################
+# Getting configurations #
+##########################
+config_dir = './config.yml'
+if os.path.basename(os.getcwd()) == 'code': config_dir = '../config.yml'
+
+# Create file if it doesnt exist
+if not os.path.isfile(config_dir):
+    with open(config_dir, 'w') as config_file:
+        config_file.write(default_config_contents)
+        print("Generated ./config.yml")
+
+# Read from config file
+with open(config_dir) as config_file:
+    parsed_config_file = yaml.load(config_file, Loader=yaml.FullLoader)
+    config_file.close()
+
+
+###########################
+# Loading config to class #
+###########################
+class Struct:
+    def __init__(self, **response):
+        for k,v in response.items():
+            if isinstance(v,dict):
+                self.__dict__[k] = Struct(**v)
+            else:
+                self.__dict__[k] = v
+
+    def __repr__(self): return '{}'.format(self.__dict__)
+
+
+# Convert dict to class object
+class config:
+    Config = Struct(**parsed_config_file)
---
+++ startup.py
@@ -0,0 +1,39 @@
+######################################
+# Import and initialize the librarys #
+######################################
+import logging
+import sys
+import os
+import pkg_resources
+
+
+##########################
+# Check for dependencies #
+##########################
+with open('requirements.txt', 'r') as requirements:
+    # Getting dependencies list needed
+    dependencies = requirements.read().split('\n')
+
+    for dependency in dependencies:
+        # Check if dependencies meets the requirements
+        try: pkg_resources.require(dependency)
+
+        # If dependencies out of date
+        except pkg_resources.VersionConflict:
+            print('Dependency {} outdated. Attempting to update now...'.format(dependency))
+            os.system('pip3 install --no-cache-dir {}'.format(dependency))
+
+        # If dependencies is not found/installed.
+        except pkg_resources.DistributionNotFound:
+            print('Dependency {} not found. Attempting to install now...'.format(dependency))
+            os.system('pip3 install --no-cache-dir {}'.format(dependency))
+
+
+###############
+# Import code #
+###############
+
+
+##############
+# Start Game #
+##############
