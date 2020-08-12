5e1310923627e45c4b61383af35c6e5dd499b83b
benwoo1110 <benedict_woo_jun_kai@s2016.sst.edu.sg>
2020-08-12 21:21:27 +0800

    Added store multiplier and tweaked game stats

--- code/logic/store.py
+++ code/logic/store.py
@@ -1,10 +1,10 @@
 ######################################
 # Import and initialize the librarys #
 ######################################
-import random
-from code.api.core import os, log, screens, coreFunc, pg
+from code.api.core import os, log, screens, pg
 from code.api.actions import Alert, Runclass
 from code.logic.player import player
+from code.logic.difficulty import difficulty
 
 
 #################
@@ -41,10 +41,12 @@ class store:
 
     @staticmethod
     def setWeapons():
+        store.weapons = pg.loadJson('./gamefiles/weapons.json')
         store_surface = screens.shop.store
 
+        # Get multiplier
+        price_multiplier = difficulty.get()['price_multiplier']
 
-        print(player.weapon.weapons)
         # Load up weapons stats to screen
         for name, weapon_data in store.weapons.items():
             if name in player.weapon.weapons: 
@@ -55,20 +57,23 @@ class store:
 
             # Set weapon text
             store_surface[name].object.setText(name.capitalize(), withDisplay=False)
-            store_surface[name].price.setText(str(weapon_data['price']), withDisplay=False)
+            store_surface[name].price.setText(str(int(weapon_data['price'] * price_multiplier)), withDisplay=False)
             store.setGain(store_surface[name].gain_1, weapon_data['gain_1'])
             store.setGain(store_surface[name].gain_2, weapon_data['gain_2'])
 
             store_surface.load(withItems='all', refresh=True)
 
     @staticmethod
+    def getPrice(weapon:str): return int(screens.shop.store[weapon].price.text)
+
+    @staticmethod
     def checkBuy(weapon:str):
         # If player have enough elixir
-        if player.stats.elixir >= store.weapons[weapon]['price']:
+        if player.stats.elixir >= store.getPrice(weapon):
             Alert(
                 type = 'confirm',
                 title = 'Buy {}'.format(weapon.capitalize()),
-                content = 'Are you such you want to buy the {} for {} elixir?'.format(weapon.capitalize(), store.weapons[weapon]['price']),
+                content = 'Are you such you want to buy the {} for {} elixir?'.format(weapon.capitalize(), store.getPrice(weapon)),
                 yes = Runclass(run=store.buy, parameters={'weapon': weapon})
             ).do()
         
@@ -77,7 +82,7 @@ class store:
             Alert(
                 type = 'notify',
                 title = 'Awww Snap',
-                content = 'You do not have enough elixir. This weapon requires {} elixir!'.format(store.weapons[weapon]['price']),
+                content = 'You do not have enough elixir. This weapon requires {} elixir!'.format(store.getPrice(weapon)),
             ).do()
 
     @staticmethod
@@ -87,7 +92,7 @@ class store:
     @staticmethod
     def buy(weapon:str):
         # Pay the price
-        player.stats.update('elixir', -store.weapons[weapon]['price'],  screens.shop.store.stats, True)
+        player.stats.update('elixir', -store.getPrice(weapon),  screens.shop.store.stats, True)
 
         # Add gains to player stats
         store.addGain(store.weapons[weapon]['gain_1'])
